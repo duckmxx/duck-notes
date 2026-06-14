f6b368b6eb6820cc7622fa8d63674dd52471597d
duckmxx <noobster611@gmail.com>
2026-06-14 00:28:03 -0700

    Checks for file and if theres a list inside it. Added deletion by index (fragile)

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import json
+import os
 
-# note1 = {"title": "My first note", "content": "learning json parsing"}
 file_path = "notes.json"
+
+def check_create_file(file_path):
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            json.dump([], file)
+    try:
+        read_json(file_path)
+    except json.decoder.JSONDecodeError:
+        with open(file_path, "w") as file:
+            json.dump([], file)
 
 def read_json(file_path):
     with open(file_path, "r") as file:
@@ -9,6 +19,9 @@ def read_json(file_path):
 
 def display_json(file_path):
     notes = read_json(file_path)
+    if not notes:               # Empty list evaluates to falsy
+        print("Wow, so empty.")
+        return
     for index, note in enumerate(notes):
         print(f"Notes entry number {index + 1}.")
         print(f"Title: {note['title']}")
@@ -22,20 +35,41 @@ def save_json(file_path, title, content):
     with open(file_path, "w") as file:
         json.dump(notes, file, indent=2)
 
+
+def delete_note(file_path, note_index):
+    notes = read_json(file_path)
+    if not notes:
+        print("Wow, can't delete nothing can you")
+        return
+    else:
+        note_to_delete = notes[note_index - 1]
+        print(f'Deleting note "{note_to_delete["title"]}"')
+        notes.pop(note_index - 1)
+        with open(file_path, "w") as file:
+            json.dump(notes, file, indent=2)
+        
+
+check_create_file(file_path)
+
 while True:
     print("1. Read existing notes")
     print("2. Write a note")
+    print("3. Delete note")
     print("Q. Quit")
     choice = input("Please enter your choice: ")
     
-    if choice == "1":
-        display_json(file_path)
-        print()
-    elif choice == "2":
-        title = input("What is the title?: ")
-        content = input("What is the contents?: ")
-
-        save_json(file_path, title, content)
-        print()
-    else: 
-        break
+    match choice.lower():
+        case "1":
+            display_json(file_path)
+            print()
+        case "2":
+            title = input("What is the title?: ")
+            content = input("What is the contents?: ")
+            save_json(file_path, title, content)
+        case "3":
+            note_index = int(input("What note would you like to delete?: "))
+            delete_note(file_path, note_index)
+        case "q":
+            break
+        case _:
+            print("Invalid choice.")
